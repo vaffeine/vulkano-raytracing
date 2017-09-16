10e2d7cecf32f6a02b379b649b6371388a8fd0ff
Vlad Alex <vlad.al.dp@gmail.com>
2017-09-17 01:07:11 +0300

    Removed outdated comments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-// Copyright (c) 2016 The vulkano developers
-// Licensed under the Apache License, Version 2.0
-// <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT
-// license <LICENSE-MIT or http://opensource.org/licenses/MIT>,
-// at your option. All files in the project carrying such
-// notice may not be copied, modified, or distributed except
-// according to those terms.
-
 #![feature(repr_align)]
 #![feature(attr_literals)]
 
@@ -146,7 +137,6 @@ fn main() {
         &graphics.images,
     );
 
-    // TODO: move frambuffers to Graphics part
     let mut previous_frame_end = Box::new(now(device.clone())) as Box<GpuFuture>;
 
     let mut fps_counter = FPSCounter::new(fps_counter::Duration::milliseconds(100));
